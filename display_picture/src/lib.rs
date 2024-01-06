e7f1ec47896968e028db8e4f9d15649784bdd1bc
minami20230128 <south614fruit@yahoo.co.jp>
2024-01-06 18:04:22 +0900

    完成

--- display_picture/src/lib.rs
+++ display_picture/src/lib.rs
@@ -1,13 +1,14 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
-pub fn main_js() -> Result<(), JsValue> {
+pub fn embed_picture() -> Result<(), JsValue> {
 
     let document = web_sys::window().unwrap().document().unwrap();
-    
-    let parent = document.get_element_by_id("parent").unwrap();
-    let child = document.create_element("img").unwrap();
 
+    let parent = document.get_element_by_id("parent").unwrap();
+    //let val = document.create_element("p").unwrap();
+    //val.set_inner_html("Hello World from WebAssemblyMan!");
+    let child = document.create_element("img").unwrap();
     child.set_attribute("src", "dog.png");
     parent.append_child(&child).unwrap();
 
