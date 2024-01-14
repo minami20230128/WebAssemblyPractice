ac179ab0fdf8962c2fbf0334844b9a50c39a6242
minami20230128 <south614fruit@yahoo.co.jp>
2024-01-14 22:59:49 +0900

    画像をリンクに変更　途中

--- display_picture/src/lib.rs
+++ display_picture/src/lib.rs
@@ -5,12 +5,17 @@ pub fn embed_picture() -> Result<(), JsValue> {
 
     let document = web_sys::window().unwrap().document().unwrap();
 
-    let parent = document.get_element_by_id("parent").unwrap();
+    let div = document.get_element_by_id("div").unwrap();
     //let val = document.create_element("p").unwrap();
     //val.set_inner_html("Hello World from WebAssemblyMan!");
-    let child = document.create_element("img").unwrap();
-    child.set_attribute("src", "dog.png");
-    parent.append_child(&child).unwrap();
+    let a = document.create_element("a").unwrap();
+    a.set_attribute("href", "https://ja.wikipedia.org/wiki/甲斐犬");
+
+    let img = document.create_element("img").unwrap();
+    img.set_attribute("src", "dog.png");
+
+    a.append_child(&img).unwrap();
+    div.append_child(&a).unwrap();
 
     Ok(())
 }
