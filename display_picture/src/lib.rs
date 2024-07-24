a2eaf8dc8fe0d00069317b6b3dada7f400519864
minami20230128 <south614fruit@yahoo.co.jp>
2024-07-24 23:55:41 +0900

    クリックされたら画像を消す処理 途中

--- display_picture/src/lib.rs
+++ display_picture/src/lib.rs
@@ -1,6 +1,8 @@
 use wasm_bindgen::prelude::*;
 extern crate console_error_panic_hook;
 use std::panic;
+use wasm_bindgen::JsCast;
+use web_sys::{console, Event, HtmlImageElement};
 
 #[wasm_bindgen(start)]
 pub fn embed_picture() -> Result<(), JsValue> {
@@ -18,8 +20,26 @@ pub fn embed_picture() -> Result<(), JsValue> {
     let img = document.create_element("img").unwrap();
     img.set_attribute("src", "dog.png");
 
+    let img_clone = img.clone(); // Clone img for closure
+    img.add_event_listener_with_callback(
+        "click",
+        // Closure handles the event
+        Closure::wrap(Box::new(move |_event: Event| {
+            // Example action on click, like logging
+            console::log_1(&"Image clicked!".into());
+        })),
+    )?;
+
     a.append_child(&img).unwrap();
     div.append_child(&a).unwrap();
 
     Ok(())
+}
+
+pub fn clear_picture(){
+    let document = web_sys::window().unwrap().document().unwrap();
+    let img = document.get_element_by_id("dog.png").unwrap();
+    if let Some(parent_node) = img.parent_node() {
+        parent_node.remove_child(&img).unwrap();
+    }
 }
