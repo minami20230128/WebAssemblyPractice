1fed5c28e19528498d7188b8a79b3fe61a54425c
minami20230128 <south614fruit@yahoo.co.jp>
2024-07-27 23:53:35 +0900

    JSコードRust化

--- display_picture/src/lib.rs
+++ display_picture/src/lib.rs
@@ -1,6 +1,8 @@
 use wasm_bindgen::prelude::*;
 extern crate console_error_panic_hook;
 use std::panic;
+use web_sys::{HtmlElement, NodeList};
+
 
 #[wasm_bindgen(start)]
 pub fn embed_picture() -> Result<(), JsValue> {
@@ -21,6 +23,8 @@ pub fn embed_picture() -> Result<(), JsValue> {
     a.append_child(&img).unwrap();
     div.append_child(&a).unwrap();
 
+    put_buttons(4);
+
     Ok(())
 }
 
@@ -32,15 +36,79 @@ pub fn clear_picture(){
     }
 }
 
-#[wasm_bindgen]
-pub fn hide_button() -> Result<(), JsValue> {
-    let window = web_sys::window().unwrap();
-    let document = window.document().unwrap();
-    let button = document.get_element_by_id("buttonToDelete");
+// グローバル変数としてカウントを保持する
+static mut COUNT: u32 = 0;
 
-    if let Some(button) = button {
-        button.remove();
+// ボタンを作成する関数
+#[wasm_bindgen]
+pub fn put_button() -> Result<(), JsValue> {
+    unsafe {
+        let document = web_sys::window().unwrap().document().unwrap();
+
+        // 新しいボタンを作成
+        let button = document.create_element("button")?.dyn_into::<HtmlElement>()?;
+        button.set_id("add_soft_button");
+        button.set_inner_html(&COUNT.to_string());
+
+        // ボタンにクリックイベントを設定
+        let button_clone = button.clone();
+        let closure = Closure::wrap(Box::new(move || {
+            event();
+        }) as Box<dyn Fn()>);
+        button.set_onclick(Some(closure.as_ref().unchecked_ref()));
+        closure.forget(); // ClosureをJavaScriptで保持させる
+
+        // <body>にボタンを追加
+        document.body().unwrap().append_child(&button)?;
+
+        // カウントを増加
+        COUNT += 1;
+    }
+    Ok(())
+}
+
+// イベントを処理する関数
+#[wasm_bindgen]
+pub fn event() -> Result<(), JsValue> {
+    remove_all_buttons()?;
+    put_buttons(4)
+}
+
+// ページ内のすべてのボタンを削除する関数
+#[wasm_bindgen]
+pub fn remove_all_buttons() -> Result<(), JsValue> {
+    let document = web_sys::window().unwrap().document().unwrap();
+
+    // ページ内のすべてのボタン要素を取得
+    let buttons:NodeList = document.query_selector_all("button").unwrap();
+
+    // ボタン要素を削除する
+    for i in (0..buttons.length()).rev() {
+        if let Some(button) = buttons.get(i) {
+            if let Some(button_element) = button.dyn_ref::<HtmlElement>() {
+                button_element.remove();
+            }
+        }
     }
 
     Ok(())
 }
+
+// 指定された数のボタンを追加する関数
+#[wasm_bindgen]
+pub fn put_buttons(num: u32) -> Result<(), JsValue> {
+    for _ in 0..num {
+        put_button()?;
+    }
+    Ok(())
+}
+
+//流れ
+//ボタンを4つ表示
+//ボタンを押す
+//すべてのボタンを消去
+//次のボタンの表示・イベントを設定
+//次のボタンを表示
+
+//init()で最初のボタンの表示・処理を設定・設置
+//
