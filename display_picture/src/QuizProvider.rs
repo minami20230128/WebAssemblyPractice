c541395d8321b23ac2211b2abec9075fab10ec16
minami20230128 <south614fruit@yahoo.co.jp>
2024-09-19 19:00:00 +0900

    QuizProvider修正

--- display_picture/src/QuizProvider.rs
+++ display_picture/src/QuizProvider.rs
@@ -1,13 +1,29 @@
+pub struct Quiz {
+    pub question: String,
+    pub options: Vec<String>,
+    pub correct_answer: String,
+}
+
+impl Quiz {
+    pub fn new() -> Self {
+        Quiz {
+            question: String::new(),
+            options: Vec::new(),
+            correct_answer: String::new(),
+        }
+    }
+}
+
 pub struct QuizProvider {
-    pub quizzes : Mutex<Vec<Quiz>>,
+    quizzes : Mutex<Vec<Quiz>>,
 }
 
 static INSTANCE: QuizProvider = QuizProvider {
-    pub quizzes: Mutex::new(Vec::new()),
+    quizzes: Mutex::new(Vec::new()),
 };
 
 impl QuizProvider {
-    load_quizzes(&self, data: JsValue){
+    pub fn load_quizzes(&self, data: JsValue){
         let vec: Vec<Quiz> = from_value(data).expect("Failed to parse JSON");
         let mut quizzes = self.quizzes.lock.unwrap();
         *quizzes = vec;
